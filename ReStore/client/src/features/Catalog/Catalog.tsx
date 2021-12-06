3a1826848669558370359eb5ca32be6712678c18
kaly <kalimamadou@gmail.com>
2021-12-05 23:30:27 -0800

    - Separate component form catalog to Product list  and Product card

--- ReStore/client/src/features/Catalog/Catalog.tsx
+++ ReStore/client/src/features/Catalog/Catalog.tsx
@@ -1,12 +1,6 @@
-import {
-  Avatar,
-  Button,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { Product } from "../../app/models/product";
+import ProductList from "./ProductList";
 
 interface Props {
   products: Product[];
@@ -16,18 +10,7 @@ interface Props {
 export default function Catalog({ products, addProduct }: Props) {
   return (
     <>
-      <List>
-        {products.map((product) => (
-          <ListItem key={product.id}>
-            <ListItemAvatar>
-              <Avatar src={product.pictureUrl} />
-            </ListItemAvatar>
-            <ListItemText>
-              {product.name} - {product.price}
-            </ListItemText>
-          </ListItem>
-        ))}
-      </List>
+      <ProductList products={products} />
       <Button variant="outlined" onClick={addProduct}>
         Add product
       </Button>
